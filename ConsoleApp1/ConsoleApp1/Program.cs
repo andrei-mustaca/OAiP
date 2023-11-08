0b70676ec18a1be86f4d36b78d759cea4400c1ad
andrei-mustaca <andrejmustaca6@gmail.com>
2023-11-09 00:41:32 +0300

    Create Program.cs

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+        //    Console.Write("Ввести x1=");
+        //    int x1 = Convert.ToInt32(Console.ReadLine());
+        //    Console.Write("Ввести y1=");
+        //    int y1 = Convert.ToInt32(Console.ReadLine());
+        //    Console.Write("Ввести x2=");
+        //    int x2 = Convert.ToInt32(Console.ReadLine());
+        //    Console.Write("Ввести y2=");
+        //    int y2 = Convert.ToInt32(Console.ReadLine());
+        //    Console.Write("Ввести x3=");
+        //    int x3 = Convert.ToInt32(Console.ReadLine());
+        //    Console.Write("Ввести y3=");
+        //    int y3 = Convert.ToInt32(Console.ReadLine());
+            
+        //    double A = Storona(x1, y1, x2, y2);double B = Storona(x2,y2,x3,y3);double C = Storona(x3,y3,x1,y1);
+        //    Compar(A, B, C);
+        //    Console.WriteLine($"Сторона равна:{A:f2}");
+        //    Console.WriteLine($"Периметр равен:{Perimetr(A,B,C):f2}");
+        //    Console.WriteLine($"Площадь равна:{S(A,B,C):f2}");
+        //    Console.ReadKey();
+        //}
+
+        //public static double Storona(double x1,double y1,double x2,double y2)
+        //{
+        //    return Math.Sqrt((Math.Pow(x2 - x1, 2)) + (Math.Pow(y2-y1,2)));
+        //}
+
+        //public static void Compar(double A,double B,double C)
+        //{
+        //    if(A<B+C&&B<A+C&&C<A+B)
+        //    {
+        //        Console.WriteLine("Треугольник существует");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Треугольник не существует");
+        //    }
+        //}
+
+        //public static double Perimetr(double A,double B,double C)
+        //{
+        //    return A + B + C;
+        //}
+
+        //public static double S(double A,double B,double C)
+        //{
+        //    double p = (A+B+C) / 2;
+        //    return Math.Sqrt(p*(p-A)*(p-B)*(p-C));
+        //}
+    }
+}
